fcf7a1d4cabf336fc09b9bce754c2357df195eba
Fabio Baltieri <fabio.baltieri@gmail.com>
2013-07-24 17:36:25 +0200

    add license information

--- toyothack.c
+++ toyothack.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2013 Fabio Baltieri <fabio.baltieri@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
